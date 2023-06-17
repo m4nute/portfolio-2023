d550e8b58d1be98a7a3f9c00e274e8db0cb4b3a7
m4nute <mgudinocontacto@gmail.com>
2023-06-17 17:25:39 -0300

    Edge functions

--- src/pages/cv.tsx
+++ src/pages/cv.tsx
@@ -1,3 +1,7 @@
+export const config = {
+  runtime: "edge"
+}
+
 export default function cv() {
   return <embed src="/cv.pdf" type="application/pdf" width="100%" height="100%" />
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,18 @@
-import { Transition, Box } from "@mantine/core";
-import useLanguage from "hooks/useLanguage";
-import { useEffect, useState } from "react";
-import Intro from "components/homepage/Intro";
-import Projects from "components/homepage/Projects";
-import Stack from "components/homepage/Stack";
-import Contact from "components/homepage/Contact";
+import { Transition, Box } from "@mantine/core"
+import useLanguage from "hooks/useLanguage"
+import { useEffect, useState } from "react"
+import Intro from "components/homepage/Intro"
+import Projects from "components/homepage/Projects"
+import Stack from "components/homepage/Stack"
+import Contact from "components/homepage/Contact"
+
+export const config = {
+  runtime: "edge"
+}
 
 export default function Home({ browserLanguage }: { browserLanguage: string }) {
-
   const { lang } = useLanguage()
-  const [spanish, setSpanish] = useState(browserLanguage === 'ES')
+  const [spanish, setSpanish] = useState(browserLanguage === "ES")
 
   const [mounted, setMounted] = useState(false)
 
@@ -17,32 +20,43 @@ export default function Home({ browserLanguage }: { browserLanguage: string }) {
     setMounted(true)
   }, [])
 
-
   return (
-    <Box sx={{
-      '@media (min-width: 600px)': { padding: '0 16px' }
-    }}>
-      <Transition mounted={mounted && lang === 'ES' && spanish} transition='fade' duration={400} onExited={() => setSpanish(false)}>
-        {(styles) => <div style={styles}>
+    <Box
+      sx={{
+        "@media (min-width: 600px)": { padding: "0 16px" }
+      }}
+    >
+      <Transition
+        mounted={mounted && lang === "ES" && spanish}
+        transition="fade"
+        duration={400}
+        onExited={() => setSpanish(false)}
+      >
+        {(styles) => (
+          <div style={styles}>
+            <Intro spanish />
+            <Projects spanish />
+            <Stack spanish />
+            <Contact spanish />
+          </div>
+        )}
+      </Transition>
 
-          <Intro spanish />
-          <Projects spanish />
-          <Stack spanish />
-          <Contact spanish />
-
-        </div>}
-      </Transition >
-
-      <Transition mounted={mounted && lang === 'EN' && !spanish} transition='fade' duration={400} onExited={() => setSpanish(true)}>
-        {(styles) => <div style={styles}>
-
-          <Intro />
-          <Projects />
-          <Stack />
-          <Contact />
-
-        </div>}
-      </Transition >
+      <Transition
+        mounted={mounted && lang === "EN" && !spanish}
+        transition="fade"
+        duration={400}
+        onExited={() => setSpanish(true)}
+      >
+        {(styles) => (
+          <div style={styles}>
+            <Intro />
+            <Projects />
+            <Stack />
+            <Contact />
+          </div>
+        )}
+      </Transition>
     </Box>
-  );
-};
+  )
+}
